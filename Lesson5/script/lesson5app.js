8f33e2d96f444c74fa9dd44f3b7a3862a357f783
rozhhole <117ywfree@gmail.com>
2021-02-04 15:47:51 +0300

    addedTaskFromPresentation

--- Lesson5/script/lesson5app.js
+++ Lesson5/script/lesson5app.js
@@ -25,4 +25,34 @@ const compact = (array) => {
    console.log(compact(data)) // [1, 2, 3]
 
 
-   
+
+//---------------------------------------------
+
+let usersAge = {
+    ivanov: "19",
+    petrov: "17",
+    sidorov: "21",
+  };
+
+  let adult = []
+  let notAnAdult = []
+
+  
+let someArr = Object.entries(usersAge)
+
+  for (let [key, value] of someArr) {
+    let someObj = {
+        name: key,
+        age: value,
+    }
+    
+    if(value>=18){
+        adult.push(someObj)
+    }
+    if(value<18){
+        notAnAdult.push(someObj)
+    }
+
+  }
+  console.log(adult)
+  console.log(notAnAdult)
